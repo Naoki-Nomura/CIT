65b6818589cbc5c73fd6c430dc9a6f5da9a269c4
Naoki-Nomura <128701092+Naoki-Nomura@users.noreply.github.com>
2023-11-17 20:04:10 +0900

    Add files via upload
    CIDebug.vdoubleのパラメータを修正

--- 20231011A/source/thesis/double/param01.c
+++ 20231011A/source/thesis/double/param01.c
@@ -99,59 +99,59 @@ void	main() {
 
 	ans = func00();		
 	#ifdef _DEBUG
-		CIDebug.vdouble("func00 result1 >>>",ans,100.0f);
+		CIDebug.vdouble("func00 result1 >>>",ans,100.0);
 	#endif
 
 	ans = func01(100.0);		
 	#ifdef _DEBUG
-		CIDebug.vdouble("func01 result1 >>>",ans,100.0f);
+		CIDebug.vdouble("func01 result1 >>>",ans,100.0);
 	#endif
 
 	ans = func02(100.0,200.0);		
 	#ifdef _DEBUG
-		CIDebug.vdouble("func02 result1 >>>",ans,300.0f);
+		CIDebug.vdouble("func02 result1 >>>",ans,300.0);
 	#endif
 
 	ans = func03(100.0,200.0,300.0);		
 	#ifdef _DEBUG
-		CIDebug.vdouble("func03 result1 >>>",ans,600.0f);
+		CIDebug.vdouble("func03 result1 >>>",ans,600.0);
 	#endif
 
 	ans = func04(100.0,200.0,300.0,400.0);		
 	#ifdef _DEBUG
-		CIDebug.vdouble("func04 result1 >>>",ans,1000.0f);
+		CIDebug.vdouble("func04 result1 >>>",ans,1000.0);
 	#endif
 
 	ans = func05(100.0,200.0,300.0,400.0,500.0);		
 	#ifdef _DEBUG
-		CIDebug.vdouble("func05 result1 >>>",ans,1500.0f);
+		CIDebug.vdouble("func05 result1 >>>",ans,1500.0);
 	#endif
 
 	ans = func06(100.0,200.0,300.0,400.0,500.0,600.0);		
 	#ifdef _DEBUG
-		CIDebug.vdouble("func06 result1 >>>",ans,2100.0f);
+		CIDebug.vdouble("func06 result1 >>>",ans,2100.0);
 	#endif
 
 	ans = func07(100.0,200.0,300.0,400.0,500.0,600.0,700.0);		
 	#ifdef _DEBUG
-		CIDebug.vdouble("func07 result1 >>>",ans,2800.0f);
+		CIDebug.vdouble("func07 result1 >>>",ans,2800.0);
 	#endif
 
 	ans = func08(100.0,200.0,300.0,400.0,500.0,600.0,700.0,800.0);		
 	#ifdef _DEBUG
-		CIDebug.vdouble("func08 result1 >>>",ans,3600.0f);
+		CIDebug.vdouble("func08 result1 >>>",ans,3600.0);
 	#endif
 
 	ans = func09(100.0,200.0,300.0,400.0,500.0,600.0,700.0,800.0,900.0);		
 	#ifdef _DEBUG
-		CIDebug.vdouble("func09 result1 >>>",ans,4500.0f);
+		CIDebug.vdouble("func09 result1 >>>",ans,4500.0);
 	#endif
 
 	ans = func10(100.0,200.0,300.0,400.0,500.0,600.0,700.0,800.0,900.0,1000.0);
 //	ans = func10(val01,val02,val03,val04,val05,val06,val07,val08,val09,val10);		
 	
 	#ifdef _DEBUG
-		CIDebug.vdouble("func10 result1 >>>",ans,5500.0f);
+		CIDebug.vdouble("func10 result1 >>>",ans,5500.0);
 	#endif
 
 //	ans = func10a(100.0,200.0,300.0,400.0,500.0,600.0,700.0,800.0,900.0,1000.0,1);
